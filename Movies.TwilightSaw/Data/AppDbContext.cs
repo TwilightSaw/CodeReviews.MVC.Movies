668b7fc7aca361a0c27a802979f570b3dd668f65
TwilightSaw <56154417+TwilightSaw@users.noreply.github.com>
2025-01-01 19:45:01 +0100

    Movies 0.01
    Started the project, created necessary classes and files

---
+++ Movies.TwilightSaw/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movies.TwilightSaw.Models;
+
+namespace Movies.TwilightSaw.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Movie> Movies { get; set; }
+
+    private readonly IConfiguration _configuration;
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Movie>();
+    }
+}
